0635d5c17cffe4b12d2b64783d5fd0ed4ecdfbe4
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-24 23:55:47 +0530

    Implement SignUp Functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,49 @@
-const signup = (req, res) => {
-  res.send("SignUp Route");
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+    if (password.length < 7) {
+      return res
+        .status(400)
+        .json({ message: "Password must contain at least 7 characters" });
+    }
+    const user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.send(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 const login = (req, res) => {
   res.send("Login Route");
