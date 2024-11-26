ffbd46befe154ff368c6fb7e98c03d5166d8deb4
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-26 18:52:54 +0530

    Implement Login, Logout & Update Profile functionalities

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 
 const signup = async (req, res) => {
@@ -45,11 +46,66 @@ const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-const login = (req, res) => {
-  res.send("Login Route");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 const logout = (req, res) => {
-  res.send("Logout Route");
+  // just clear the cookies
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged Out Successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-export { signup, login, logout };
+const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in updateProfile controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export { signup, login, logout, updateProfile, checkAuth };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -9,6 +10,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
