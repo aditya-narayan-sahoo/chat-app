f0c9885b0aacc8981fc68978b5401cd13f18fcbf
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-27 01:03:24 +0530

    Add the message controllers & routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/auth/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
