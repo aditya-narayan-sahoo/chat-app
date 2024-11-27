2eee15264158c98892f76c15732f3398b47003b1
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-27 14:10:50 +0530

    Init App Layout & add routing config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>Frontend Part</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
