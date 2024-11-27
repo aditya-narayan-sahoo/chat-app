085c33b2d2d66c253c1a1cf6c1ea794a36af6658
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-27 18:25:52 +0530

    Allow requests from frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
@@ -12,6 +13,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/auth/message", messageRoutes);
