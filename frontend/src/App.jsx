a949dd3432a1545270f9bf77e5e71f770c00ee62
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-27 18:30:00 +0530

    Modify App Layout to check for Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,53 @@
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
-import { Routes, Route } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
         <Route path="/settings" element={<SettingsPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 };
