af8ddadb52db97e972b827a3463052e00f7b8e29
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-28 13:29:46 +0530

    Modify the home page layout

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import Sidebar from "../components/Sidebar";
+import { useChatStore } from "../store/useChatStore";
+import ChatContainer from "../components/ChatContainer";
+import NoChatSelected from "../components/NoChatSelected";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { selectedUser } = useChatStore();
+
+  return (
+    <main className="h-screen bg-base-200">
+      <section className="flex items-center justify-center pt-20 px-4">
+        <article className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
