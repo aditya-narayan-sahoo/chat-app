f5f27fd779b0531657beaaf8314216cbb9212fdd
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-11-28 21:11:37 +0530

    Configure socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./lib/socket.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
 
@@ -23,7 +23,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
   connectDB();
 });
